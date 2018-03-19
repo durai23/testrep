861c43c8a4c260155e302b347de34ec7112f34e7
Durai Arasan <dnambil@imenb050.ime.kfa-juelich.de>
2018-03-19 02:10:20 +0100

    psmd plots

--- psmd/psmd_plots.py
+++ psmd/psmd_plots.py
@@ -1,5 +1,4 @@
-#import matplotlib.pyplot as plt
-#see if thsi shows up in imecas
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
@@ -8,20 +7,40 @@ import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 import subprocess
-
+#get hostname and set base directory
 j = subprocess.Popen("hostname", stdout=subprocess.PIPE, shell=True)
 (output, err) = j.communicate()
-
 if 'imen' in output:
     base="/home/dnambil/Downloads/BRIDGET/plots"
 else:
     base="/data/Team_Caspers/Arasan/BRIDGET/1000Brains_b1000-dMRI030_99xxxx_dwi_eddy"
 
 print base
-
+#read in 1000 BRAINS subject list
 dfs=pd.read_table(base+'/Visit1_Age.txt',sep='\t')
+#read in PSMD subjects list
 dfm=pd.read_csv(base+'/TOTAL_METRICS_Skel_header.csv',sep=' ')
+#merge keeps both key columns - figure out how to avoid this
+dfplot=dfm.merge(dfs,left_on='NAME', right_on='Identifiers',sort=True)
+df_plot=dfplot.drop('Identifiers',axis=1)
+df_plot_no_visit=df_plot.drop('Visit',axis=1)
+#sns.heatmap(df_plot_no_visit.corr(),xticklabels=df_plot_no_visit.corr().columns.values,yticklabels=df_plot_no_visit.corr().columns.values)
 
+def dia_corr_mat():
+    df_plot_no_visit_no_name=df_plot_no_visit.drop('NAME',axis=1)
+    corr=df_plot_no_visit_no_name.corr()
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+    #f, ax = plt.subplots()
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap,annot=True, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    
 #function to list subjects in PSMD results but not in 1000Brains list
 def missing_subs():
     for i in dfm.NAME:
@@ -37,7 +56,7 @@ def get_age_metric(metric):
             y=np.append(y,dfm[dfm.NAME==i][metric].values[0])
     return x,y
 
-#plots metrics
+#plots line reg params - metric vs age
 def plot_metric(metric):
     x,y=get_age_metric(metric)
     data = pd.DataFrame(data={'Age': x, metric: y})
@@ -48,10 +67,10 @@ def plot_metric(metric):
     
     #sns.regplot(x='Age',y=metric,data=data)
     sns.jointplot(x='Age',y=metric,data=data,kind='reg')
-    
-def plot_metric2(metric):
-    x,y=get_age_metric(metric)
-    
+
+def plot_metric_2(metric):
+    print metric
+    sns.jointplot(x='Age',y=metric,data=df_plot_no_visit,kind='reg')
     
 def scp_linreg_metric(metric):
     x,y=get_age_metric(metric)
@@ -64,8 +83,6 @@ def sm_ols_metric(metric):
     #predictions = model.predict(x) # make the predictions by the model
     # Print out the statistics
     print model.summary()
-    #slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    #return slope, r_value**2, p_value
     
 def skl_linreg_metric(metric):
     x,y=get_age_metric(metric)
@@ -74,13 +91,7 @@ def skl_linreg_metric(metric):
     model=LinearRegression()
     model.fit(x,y)
     print model.score(x,y)
-
-#def parse_metric(metric):
-#    if metric=='mean_skel_MD_LH_RH':
-#        return mean_MD
-#    elif metric=='sd_skel_MD_LH_RH':
-#        return sd_MD
-    
+  
 def make_df_reg_all(sd=True):    
     df_list=[]
     for i in dfm.columns.values.tolist():
@@ -102,7 +113,7 @@ def make_df_reg_all(sd=True):
     df=pd.DataFrame(df_list)
     print df
     df.to_csv(base+'/linreg_metrics.csv')
-    
+   
 def polyfit2(x, y, degree):
     results = {}
     coeffs = np.polyfit(x, y, degree)
@@ -117,4 +128,28 @@ def polyfit2(x, y, degree):
     sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
     results['determination'] = ssreg / sstot
     return results
-        
+
+#def parse_metric(metric):
+#    if metric=='mean_skel_MD_LH_RH':
+#        return mean_MD
+#    elif metric=='sd_skel_MD_LH_RH':
+#        return sd_MD    .
+    
+#def df_corr_all():
+#    df_corr_list=[]
+#    x=np.array([])
+#    y=np.array([])
+#    for i in dfm.NAME:
+#        if dfs[dfs.Identifiers==i]['Identifiers'].values.shape[0]>0:
+#            x=np.append(x,dfs[dfs.Identifiers==i]['Age'].values[0])		
+#            for j in dfm.columns.values.tolist():
+#                if "LH" in j:
+#                    y=np.append(y,dfm[dfm.NAME==i][j].values[0])
+#
+#                    
+#            y=np.append(y,dfm[dfm.NAME==i][metric].values[0])
+#    
+#    Age,_ = get_age_metric()
+#    for i in dfm.NAME:
+#        if 
+#        row_dict={'Age':}

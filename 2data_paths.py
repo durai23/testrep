a858b1d920136583cd67b36c1b4068c310f34520
lol man <lmaoman@example.com>
2018-12-02 07:15:15 +0100

    2datapaths working full function :)

--- 2data_paths.py
+++ 2data_paths.py
@@ -1,8 +1,9 @@
 import os
 import argparse
 import pandas as pd
+import time
 
-printlist=pd.read_csv(r'C:\Users\synapse\Desktop\11_21_2018_data_paths\print_list.csv')
+# printlist=pd.read_csv(r'C:\Users\synapse\Desktop\11_21_2018_data_paths\print_list.csv')
 
 # if printlist.include
 # Instantiate the parser
@@ -31,11 +32,32 @@ print(args.out_file)
 # THEN add pandas metadata saving
 def print_paths_and_log_to_csv(in_dir):
 	f_count=0
+	cols=['fpath','size','date_created','date_modified']
+	in_dir_base=os.path.basename(in_dir)
+	out_csv=os.path.join("C:",os.sep,"cygwin64","home","synapse","testrep","data_paths",in_dir_base+".csv")
+	out_txt=os.path.join("C:",os.sep,"cygwin64","home","synapse","testrep","data_paths",in_dir_base+".txt")
+	df=pd.DataFrame(columns=cols)
+	a=open(out_txt,'w')
 	for path, subdirs, files in os.walk(in_dir):
 		for filename in files:
 			f=os.path.join(path,filename)
-			print f
-			
+			fsize=os.path.getsize(f)
+			fctime=time.ctime(os.path.getctime(f))
+			fmtime=time.ctime(os.path.getmtime(f))
+			df1=pd.DataFrame([[f,fsize,fctime,fmtime]],columns=cols)
+			#print f
+			# pd.concat([df,df1])
+			df=df.append(df1,ignore_index=True)
+			a.write(str(f)+",")
+			f_count+=1
+	
+
+	df.to_csv(out_csv)
+
+	print "files processes "+str(f_count)
+	# print "5 rows of df"
+	# print df.head
+
 # print "method 2"
 # print next(os.walk(args.in_folder))[1]
 # print next(os.walk(args.in_folder))[0]
